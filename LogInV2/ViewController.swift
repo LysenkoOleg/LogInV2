e85dfbea9c61a33f45fa6c01253e51d1628d1b39
Lysenko Oleg <moto.froloff@yandex.ru>
2021-08-29 19:33:49 +0300

    Initial Commit

---
+++ LogInV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LogInV2
+//
+//  Created by Олег Лысенко on 29.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
